934f838ef21f3fbeb46e08f029e4cdf316106bcb
unknown <fazahuanchef@gmail.com>
2021-11-27 03:35:08 -0500

    segundo intento autentificar para el registro

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 // Este es el punto de entrada de tu aplicacion
+// eslint-disable-next-line import/no-unresolved
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
+// eslint-disable-next-line import/no-unresolved
+import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCaQ7ypqFXHu9ikarcZpoPLKq1t5KxNmic',
+  authDomain: 'helppetsprueba2.firebaseapp.com',
+  projectId: 'helppetsprueba2',
+  storageBucket: 'helppetsprueba2.appspot.com',
+  messagingSenderId: '455441875262',
+  appId: '1:455441875262:web:660c2751c247dc85696c6f',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const registrate = document.getElementById('registrate');
 registrate.addEventListener('click', () => {
@@ -12,7 +34,18 @@ formRegistro.addEventListener('submit', (e) => {
   const usuarioRegistro = document.getElementById('usuarioRegistro').value;
   const correoRegistro = document.getElementById('correoRegistro').value;
   const claveRegistro = document.getElementById('claveRegistro').value;
-  console.log(usuarioRegistro);
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, correoRegistro, claveRegistro)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
 });
 
 const formIngreso = document.getElementById('formIngreso');
