86e0fee628586a3c44add473ae7a85b0c9daea67
unknown <fazahuanchef@gmail.com>
2021-11-27 01:36:55 -0500

    primer intento de firebase-auth

--- src/main.js
+++ src/main.js
@@ -1,11 +1,23 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
-
-myFunction();
-
 const registrate = document.getElementById('registrate');
 registrate.addEventListener('click', () => {
   document.getElementById('formRegistro').style.display = 'block';
   document.getElementById('formIngreso').style.display = 'none';
+});
+
+const formRegistro = document.getElementById('formRegistro');
+formRegistro.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const usuarioRegistro = document.getElementById('usuarioRegistro').value;
+  const correoRegistro = document.getElementById('correoRegistro').value;
+  const claveRegistro = document.getElementById('claveRegistro').value;
+  console.log(usuarioRegistro);
+});
+
+const formIngreso = document.getElementById('formIngreso');
+formIngreso.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let correoIngreso = document.getElementById('correoIngreso').value;
+  let claveIngreso = document.getElementById('claveIngreso').value;
 });
