5ecc0f41b9971229c8908814666915742c0d9adf
unknown <fazahuanchef@gmail.com>
2021-11-28 16:10:55 -0500

    arreglos en html y js y desinstalando las dependencias de firebase

--- src/main.js
+++ src/main.js
@@ -1,26 +1,3 @@
-// eslint-disable-next-line import/no-unresolved
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js';
-// eslint-disable-next-line import/no-unresolved
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
-
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyCaQ7ypqFXHu9ikarcZpoPLKq1t5KxNmic',
-  authDomain: 'helppetsprueba2.firebaseapp.com',
-  projectId: 'helppetsprueba2',
-  storageBucket: 'helppetsprueba2.appspot.com',
-  messagingSenderId: '455441875262',
-  appId: '1:455441875262:web:660c2751c247dc85696c6f',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 const registrate = document.getElementById('registrate');
 registrate.addEventListener('click', () => {
   document.getElementById('formRegistro').style.display = 'block';
@@ -30,26 +7,16 @@ registrate.addEventListener('click', () => {
 const formRegistro = document.getElementById('formRegistro');
 formRegistro.addEventListener('submit', (e) => {
   e.preventDefault();
-  const usuarioRegistro = document.getElementById('usuarioRegistro').value;
-  const correoRegistro = document.getElementById('correoRegistro').value;
-  const claveRegistro = document.getElementById('claveRegistro').value;
-  const auth = getAuth();
-  createUserWithEmailAndPassword(auth, correoRegistro, claveRegistro)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
+  const usuarioRegistro = formRegistro.usuarioRegistro.value;
+  const correoRegistro = formRegistro.correoRegistro.value;
+  const claveRegistro = formRegistro.claveRegistro.value;
+  console.log(usuarioRegistro, correoRegistro, claveRegistro);
 });
 
 const formIngreso = document.getElementById('formIngreso');
 formIngreso.addEventListener('submit', (e) => {
   e.preventDefault();
-  let correoIngreso = document.getElementById('correoIngreso').value;
-  let claveIngreso = document.getElementById('claveIngreso').value;
+  const correoIngreso = formIngreso.correoIngreso.value;
+  const claveIngreso = formIngreso.claveIngreso.value;
+  console.log(correoIngreso, claveIngreso);
 });
