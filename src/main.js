93c924b9903cd50938a7793820703c58373f3974
unknown <fazahuanchef@gmail.com>
2021-11-30 00:56:10 -0500

    correcion con eslint

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
-
 // eslint-disable-next-line import/no-unresolved
-
-//import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
-//import { app } from './firebase/config.js';
-//import { componentes } from '../lib/index.js';
-import {vistasPantalla} from '../router/rutas.js'
+// import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
+// import { app } from './firebase/config.js';
+// import { componentes } from '../lib/index.js';
+import { vistasPantalla } from './router/rutas.js';
 // Este es el punto de entrada de tu aplicacion
-import {ingreso} from '../componentes/inicioSeccion.js'
+
 window.addEventListener('load', vistasPantalla);
 
 window.addEventListener('hashchange', vistasPantalla);
