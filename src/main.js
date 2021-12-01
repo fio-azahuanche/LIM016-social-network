bfbef375f7c78d91c6486df82c326c2cdbb02f68
unknown <cruzyumari2@gmail.com>
2021-11-30 21:56:51 -0500

    Uso correcto de autenticacion con Firebase

--- src/main.js
+++ src/main.js
@@ -1,45 +1,4 @@
-
-// eslint-disable-next-line import/no-unresolved
-//import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
-//import { app } from './firebase/config.js';
-//import { componentes } from '../lib/index.js';
 import {vistasPantalla} from '../router/rutas.js'
-// Este es el punto de entrada de tu aplicacion
-import {ingreso} from '../componentes/inicioSeccion.js'
+
 window.addEventListener('load', vistasPantalla);
-
 window.addEventListener('hashchange', vistasPantalla);
-
-console.log(vistasPantalla);
-
-
-
-
-/* const formRegistro = document.querySelector('#formRegistro');
-formRegistro.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const usuarioRegistro = document.getElementById('usuarioRegistro').value;
-  const correoRegistro = document.getElementById('correoRegistro').value;
-  const claveRegistro = document.getElementById('claveRegistro').value;
-  const auth = getAuth();
-  createUserWithEmailAndPassword(auth, correoRegistro, claveRegistro)
-    .then((userCredential) => {
-      // Signed in
-      const user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
-    });
-}); */
-
-/*
-const formIngreso = document.getElementById('formIngreso');
-formIngreso.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let correoIngreso = document.getElementById('correoIngreso').value;
-  let claveIngreso = document.getElementById('claveIngreso').value;
-});
- */
--- src/router/rutas.js
+++ src/router/rutas.js
@@ -1,17 +1,18 @@
-//import { ingreso } from '../componentes/inicioSeccion.js';
 import { componentes } from '../lib/index.js';
+import { forms } from "../componentes/formulariosHome.js";
 import { divMuro } from '../componentes/muro.js';
+
+
 export const vistasPantalla = () => {
     const main = document.getElementById('main');
-    console.log(document.getElementById('formRegistro'));
     main.innerHTML = '';
     switch (window.location.hash.toLowerCase()) {
         case '': case '#/': case '#/inicio':
-
-            main.appendChild(componentes.fondoInicial(componentes.ingreso));
+            main.appendChild(componentes.fondoHome(forms.inicioSesion()));
             break;
         case '#/registro':
-            main.appendChild(componentes.fondoInicial(componentes.registro));
+            main.appendChild(componentes.fondoHome(forms.registro()));
+            registroCorreo('usuarioRegistro', 'correoRegistro', 'claveRegistro', 'formRegistro');
             break;
         case '#/artmuro':
             main.innerHTML = divMuro;
