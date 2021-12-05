ef197390ca095d4443fbe12af4e3cf87cb01e577
unknown <fazahuanchef@gmail.com>
2021-12-05 02:37:43 -0500

    corrigiendo errores del merge

--- src/componentes/registro.js
+++ src/componentes/registro.js
@@ -55,22 +55,22 @@ export const formRegistros = () => {
     <div id='registro' class='cajaInterna2'>
       <form id="formRegistro">
 
-                <div class="seccionIngreso">
-                    <input type="password" id="claveRegistro" class="datosIngreso" placeholder="Contraseña" required>
-                    <i id="botonClave" class="ph-eye-closed"></i>
-                </div>
+      <div class="seccionIngreso">
+        <input type="text" id="usuarioRegistro" class="datosIngreso" placeholder="Nombre de usuario" required>
+        <img src="imagenes/user.png">
+      </div>
 
-        <div class="seccionIngreso">
-          <input type="text" id="correoRegistro" class="datosIngreso" placeholder="Correo electrónico" required>
-          <img src="imagenes/envelope.png">
-        </div>
+      <div class="seccionIngreso">
+        <input type="text" id="correoRegistro" class="datosIngreso" placeholder="Correo electrónico" required>
+        <img src="imagenes/envelope.png">
+      </div>
+      
+      <div class="seccionIngreso">
+        <input type="password" id="claveRegistro" class="datosIngreso" placeholder="Contraseña" required>
+        <i id="botonClave" class="ph-eye-closed"></i>
+      </div>
 
-        <div class="seccionIngreso">
-          <input type="password" name="password" id="claveRegistro" class="datosIngreso" placeholder="Contraseña" required>
-          <img src="imagenes/eye-closed.png">
-        </div>
-
-        <button type="submit" class="iniciarSesion">Registrate</button>
+      <button type="submit" class="iniciarSesion">Registrate</button>
         
         <p class="texto">¿Ya tienes una cuenta? <a id="registrate" href="#/inicio"> Iniciar Sesión</a></p> 
       </form>
