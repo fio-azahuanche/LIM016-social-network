800bd99f033981a84caed1e388b35102b97b5525
Marielena <maizaga01@gmail.com>
2021-12-06 22:49:27 -0500

    ultimos cambios de la vista 2

--- src/componentes/seccionSecMuro.js
+++ src/componentes/seccionSecMuro.js
@@ -10,50 +10,57 @@ export const seccionMuro2 = () => {
             </div>                  
         </div>
 
-        <div class="tableroPost">
-            <div class="usuarioPost">
-                <div class="imgUsuarioPost"><img class="imgPost"src="imagenes/ImgUsuario3.png"></div>
-                <div class="infoUsuarioPost">
-                    <div class="nombreUsuarioPost"><p>María Casas</p><img src="imagenes/bxs-user-plus 2.png"></div>
-                    <div class="descripcionUsuarioPost"><p>catlover</p></div>                                  
+        <div class="container-post" id="container-post">
+            <div class="tableroPost">
+                <div class="usuarioPost">
+                    <div class="imgUsuarioPost"><img class="imgPost"src="imagenes/ImgUsuario3.png"></div>
+                    <div class="infoUsuarioPost">
+                        <div class="nombreUsuarioPost"><p>María Casas</p><img src="imagenes/bxs-user-plus 2.png"></div>
+                        <div class="descripcionUsuarioPost"><p>catlover</p></div>                                  
+                    </div>
+                    <div class="botonGlobo"><img src="imagenes/bx-globe.png"></div>     
                 </div>
-                <div class="botonGlobo"><img src="imagenes/bx-globe.png"></div>     
-            </div>
-            <div class="estadoCompartido">
-                <div class="contenidoCompartido">
-                    <p></p>
-                    <img src="imagenes/perritoFoto.png">
+                <div class="estadoCompartido">
+                    <div class="contenidoCompartido">
+                        <p></p>
+                        <img src="imagenes/perritoFoto.png">
+                    </div>
+                </div>  
+                <div class="botonesReaccion">
+                    <img src="imagenes/heartIcono.png">
+                    <img src="imagenes/comentIcono.png">
+                    <img src="imagenes/compartirIcono.png">
                 </div>
-            </div>  
-            <div class="botonesReaccion">
-                <img src="imagenes/heartIcono.png">
-                <img src="imagenes/comentIcono.png">
-                <img src="imagenes/compartirIcono.png">
             </div>
+
+            <div class="tableroPost">
+                <div class="usuarioPost">
+                    <div class="imgUsuarioPost"><img class="imgPost"src="imagenes/ImgUsuario2.png"></div>
+                    <div class="infoUsuarioPost">
+                        <div class="nombreUsuarioPost"><p>Julio Perez</p><img src="imagenes/bxs-user-plus 2.png"></div>
+                        <div class="descripcionUsuarioPost"><p>Voluntario en WUF</p></div>                                  
+                    </div>
+                    <div class="botonGlobo"><img src="imagenes/bx-globe.png"></div>     
+                </div>
+                <div class="estadoCompartido">
+                    <p class="contenidoCompartido">Adoptar una mascota es cambiar dos vidas: la de la mascota que al fin olvidará sus duros días 
+                    sin familia y la de quien se convertirá en su dueño y tendrá días cargados de amor. Si te interesa
+                    acoger a un nuevo miembro en tu hogar, estas son algunas de las muchas opciones que encuentras 
+                    para adoptar animales en Lima.</p>
+                    <img src="">
+                </div>  
+                <div class="botonesReaccion">
+                    <img src="imagenes/heartIcono.png">
+                    <img src="imagenes/comentIcono.png">
+                    <img src="imagenes/compartirIcono.png">
+                </div>
+            </div>           
         </div>
+
+
+
         
-        <div class="tableroPost">
-            <div class="usuarioPost">
-                <div class="imgUsuarioPost"><img class="imgPost"src="imagenes/ImgUsuario2.png"></div>
-                <div class="infoUsuarioPost">
-                    <div class="nombreUsuarioPost"><p>Julio Perez</p><img src="imagenes/bxs-user-plus 2.png"></div>
-                    <div class="descripcionUsuarioPost"><p>Voluntario en WUF</p></div>                                  
-                </div>
-                <div class="botonGlobo"><img src="imagenes/bx-globe.png"></div>     
-            </div>
-            <div class="estadoCompartido">
-                <p class="contenidoCompartido">Adoptar una mascota es cambiar dos vidas: la de la mascota que al fin olvidará sus duros días 
-                sin familia y la de quien se convertirá en su dueño y tendrá días cargados de amor. Si te interesa
-                acoger a un nuevo miembro en tu hogar, estas son algunas de las muchas opciones que encuentras 
-                para adoptar animales en Lima.</p>
-                <img src="">
-            </div>  
-            <div class="botonesReaccion">
-                <img src="imagenes/heartIcono.png">
-                <img src="imagenes/comentIcono.png">
-                <img src="imagenes/compartirIcono.png">
-            </div>
-        </div>
+       
    `;
   return segundaSeccion;
 };
