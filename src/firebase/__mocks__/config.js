37380600446633e7845a7045e818f3e497064b07
unknown <fazahuanchef@gmail.com>
2021-12-22 10:17:09 -0500

    primer intento de testeo de las funcionesAuth

--- src/firebase/__mocks__/config.js
+++ src/firebase/__mocks__/config.js
@@ -1,3 +1,11 @@
 export const signInWithEmailAndPassword = jest.fn(() => Promise.resolve({}));
-
+export const createUserWithEmailAndPassword = jest.fn(() => Promise.resolve({}));
+export const signOut = jest.fn(() => Promise.resolve({}));
+export const onAuthStateChanged = jest.fn(() => Promise.resolve({}));
+export const signInWithPopup = jest.fn(() => Promise.resolve({}));
+export const sendEmailVerification = jest.fn(() => Promise.resolve({}));
 export const auth = jest.fn();
+
+export const getDoc = jest.fn(() => Promise.resolve({}));
+
+export const db = jest.fn();
