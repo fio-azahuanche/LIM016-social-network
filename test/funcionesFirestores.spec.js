8124d33bfd96946d5feff24e7a296bab92cadea7
unknown <fazahuanchef@gmail.com>
2022-01-06 11:19:32 -0500

    agregando readme y borrando comentarios

--- test/funcionesFirestores.spec.js
+++ test/funcionesFirestores.spec.js
@@ -7,24 +7,12 @@ jest.mock('../src/firebase/config');
 
 describe('subirDataHomeCol', () => {
   it('Deberia subir data a coleccion posts', () => subirDataHomeCol('strCreador', 'strPost', 'strCat', '').then(async () => {
-    /* console.log(addDoc.mock.calls[0][1]);
-    console.log(addDoc(collection.mock.results[0].value, addDoc.mock.calls[0][1])); */
-    // console.log(addDoc.mock.results[0].value);
     const prueba = await addDoc(collection.mock.results[0].value, addDoc.mock.calls[0][1]);
     const variable = {
       posts: {
         categoria: 'strCat', imgPost: '', likes: [], publicacion: 'strPost', timestamp: undefined, usuarioId: 'strCreador',
       },
     };
-    /* expect(prueba).toBe(variable); */
     expect(prueba).toEqual(variable);
   }));
 });
-
-/* describe('agregarDataUserFS', () => {
-  it('Obtener', () => agregarDataUserFS('id',
-  'Username', 'Correo', 'Name', 'Descripcion', 'Ubicacion', '', '').then(() => {
-    console.log(doc.mock);
-    console.log(setDoc.mock);
-  }));
-}); */
